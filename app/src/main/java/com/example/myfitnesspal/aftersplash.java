dcbbc3991c5cfd4c3ddc8574fe24f10b71983304
Shrestha <shresthabanerjee@gmail.com>
2023-04-04 20:34:09 +0530

    contents page

--- app/src/main/java/com/example/myfitnesspal/aftersplash.java
+++ app/src/main/java/com/example/myfitnesspal/aftersplash.java
@@ -2,13 +2,33 @@ package com.example.myfitnesspal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class aftersplash extends AppCompatActivity {
-
+    private Button bmi;
+    private Button yoga;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_splash);
+        bmi=findViewById(R.id.button1);
+        bmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(aftersplash.this,bmi.class);
+                startActivity(intent);
+            }
+        });
+        yoga=findViewById(R.id.button2);
+        yoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(aftersplash.this,yoga_select_pose.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/myfitnesspal/yoga_select_pose.java
@@ -0,0 +1,14 @@
+package com.example.myfitnesspal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class yoga_select_pose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yoga_select_pose);
+    }
+}
