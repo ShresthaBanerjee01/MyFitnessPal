c545c7b120e680528ef61a63ce9013c9147a7f76
Shrestha <shresthabanerjee@gmail.com>
2023-04-13 01:21:55 +0530

    new activity created

--- app/src/main/java/com/example/myfitnesspal/yoga_select_pose.java
+++ app/src/main/java/com/example/myfitnesspal/yoga_select_pose.java
@@ -2,13 +2,34 @@ package com.example.myfitnesspal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class yoga_select_pose extends AppCompatActivity {
+public class yoga_select_pose extends AppCompatActivity
+{
+    int[] newArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yoga_select_pose);
+
+        newArray=new int[]{
+                R.id.boat_pose, R.id.bow_pose, R.id.cobra_pose, R.id.crescent_lunge, R.id.downward_facing_dog, R.id.easy_pose,
+                R.id.half_pegion_pose, R.id.low_lunge, R.id.upward_bow, R.id.warrior_pose, R.id.warrior_pose2
+        };
+    }
+
+    public void ImageButtonClicked(View view)
+    {
+        int i=0;
+        for(i=0;i<newArray.length;i++)
+        {
+            int value=i+1;
+            Intent intent =new Intent(yoga_select_pose.this,ThirdActivity.class);
+            intent.putExtra("Value",String.valueOf(value));
+            startActivity(intent);
+        }
     }
 }
