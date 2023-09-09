018af2e58f6329de867e2d0f478651ac906eee72
SANJINI <likupal25042002@gmail.com>
2023-09-09 22:13:56 +0530

    pedometer and calorimeter and travel

---
+++ app/src/main/java/com/example/myfitnesspal/palacesforts.java
@@ -0,0 +1,14 @@
+package com.example.myfitnesspal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class palacesforts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palacesforts);
+    }
+}
---
+++ app/src/main/java/com/example/myfitnesspal/temples.java
@@ -0,0 +1,14 @@
+package com.example.myfitnesspal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class temples extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temples);
+    }
+}
